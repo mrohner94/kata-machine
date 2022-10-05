f6065b7071cad2fbfbbfd7b472c37c00e43f43b4
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2022-10-05 09:37:35 -0400

    Update BubbleSort.ts

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,18 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
 
-	let runFunc = true;
-	do{
-		runFunc = false;
-		for(let i = 0; i < arr.length; i++) {
-			if(arr[i] > arr[i + 1]) {
-				runFunc = true;
-				let newHi = arr[i];
-				let newLo = arr[i+1];
-				
-				arr[i] = newLo;
-				arr[i + 1] = newHi;
+	for(let i = 0; i < arr.length; i++) {
+		for(let j = 0; j < arr.length - 1; j++) {
+			if(arr[j] > arr[j + 1]) {
+				const newHi = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = newHi
 			}
 		}
-	} while (runFunc)
-
+	}
 }
