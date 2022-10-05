c4ca3750af50144f8467eab937cbf203a8d7e013
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2022-10-05 09:32:38 -0400

    crystal ball, bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,18 @@
 export default function bubble_sort(arr: number[]): void {
 
+	let runFunc = true;
+	do{
+		runFunc = false;
+		for(let i = 0; i < arr.length; i++) {
+			if(arr[i] > arr[i + 1]) {
+				runFunc = true;
+				let newHi = arr[i];
+				let newLo = arr[i+1];
+				
+				arr[i] = newLo;
+				arr[i + 1] = newHi;
+			}
+		}
+	} while (runFunc)
+
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+	const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+	
+	let breakLimit = jumpAmount;
+	for(breakLimit; breakLimit < breaks.length; breakLimit += jumpAmount){
+		if(breaks[breakLimit]) {
+			break;
+		}
+	}
+	
+	for(let i = breakLimit - jumpAmount; i < breakLimit; i++) {
+		if(breaks[i]) {
+			return i;
+		}
+	}
 
+	return -1;
+
+	
 }
